e16277a6539bceef6735427323fa78f9a5e56762
JPrimo <juliozz59@gmail.com.com>
2019-08-19 21:25:43 -0700

    added jenkinsfile and backend services

---
+++ backend/build_services.sh
@@ -0,0 +1,12 @@
+#/!bin/bash
+debug="true"
+echo "----------building backend services-----------"
+if [ ${debug} = "true" ]; then
+    sleep 10
+fi
+echo "Finished buolding backend services..."
+echo ""
+echo ""
+
+
+echo "---------EXITING FILE--------"
